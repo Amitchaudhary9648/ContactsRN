49cca4216abef195c960a1e878588caf68b9163e
Amit Chaudhary <59361363+Amitchaudhary9648@users.noreply.github.com>
2022-05-26 22:05:48 +0530

    add the packages for navigation

--- android/app/src/main/java/com/contactsrn/MainActivity.java
+++ android/app/src/main/java/com/contactsrn/MainActivity.java
@@ -3,6 +3,7 @@ package com.contactsrn;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "ContactsRN";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(null);
   }
 
   /**
